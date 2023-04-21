562d66d3a2e6af991406057d4c6f07404372ea21
MatubaraTooru <kurakura.ingen@gmail.com>
2023-04-21 17:04:14 +0900

    スタックとキュー

---
+++ Paiza/Queue.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Queue
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        Queue<int> queue = new Queue<int>();
+        for (int i = 0; i < n; i++)
+        {
+            var num = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            if (num[0] == 1)
+            {
+                queue.Enqueue(num[1]);
+            }
+        }
+
+        Console.WriteLine(queue.Count);
+
+        foreach(var i in queue)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
---
+++ Paiza/Stack.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program2
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        Stack<string> stack = new Stack<string>();
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] data = Console.ReadLine().Split();
+
+            if (int.Parse(data[0]) == 1)
+            {
+                stack.Push(data[1]);
+            }
+            else if (int.Parse(data[0]) == 2)
+            {
+                Console.WriteLine(stack.First());
+                stack.Pop();
+            }
+
+            Console.WriteLine(string.Join(" ", stack.Reverse()));
+        }
+    }
+}
