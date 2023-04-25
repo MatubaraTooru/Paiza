a774fa569a3bc855b3f576345160693dcecebf7d
MatubaraTooru <kurakura.ingen@gmail.com>
2023-04-25 18:07:30 +0900

    最大の区間和

---
+++ Paiza/any.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        string[] inputs = Console.ReadLine().Split(' ');
+        Queue<int> q = new Queue<int>();
+        int[] ints = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+        int l = 0;
+        int max = 0;
+        int sum = 0;
+
+        for (int i = 0; i < ints.Length; i++)
+        {
+            q.Enqueue(ints[i]);
+            sum += ints[i];
+
+            if (q.Count >= int.Parse(inputs[1]))
+            {
+                if (max < sum)
+                {
+                    max = sum;
+                    l = q.Peek();
+                }
+                sum -= q.Dequeue();
+            }
+        }
+
+        Console.WriteLine($"{max} {l}");
+    }
+}
