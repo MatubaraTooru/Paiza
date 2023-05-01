0350a9bc73832addc8bde1d6a0d1d1d537e8fc84
MatubaraTooru <kurakura.ingen@gmail.com>
2023-05-01 21:18:58 +0900

    フェルマーテスト

---
+++ Paiza/Fermat's test.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class FermatsTest
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int a = 2;
+        int fermat = 1;
+
+        if (n % a == 0)
+        {
+            Console.WriteLine("NO");
+        }
+        for (int i = 0; i < n - 1; i++)
+        {
+            fermat *= a;
+            fermat %= n;
+        }
+        if (fermat % n == 1)
+        {
+            Console.WriteLine("YES");
+        }
+        else
+        {
+            Console.WriteLine("NO");
+        }
+    }
+}
