c2256b4c48f8cafc2891cbde4f6201bc7cc02644
MatubaraTooru <kurakura.ingen@gmail.com>
2023-05-13 23:58:38 +0900

    銀行

--- Paiza/any 2.cs
+++ Paiza/any 2.cs
@@ -5,28 +5,6 @@ class Any2
 {
     static void Main()
     {
-        long n = int.Parse(Console.ReadLine());
-        long maxnum = 0;
-        long num1 = 0;
-        long num2 = 0;
-
-        for (long i = 2; i * i < n; i++)
-        {
-            IsPrime();
-        }
-    }
-    static long IsPrime(long n)
-    {
-        if (n < 2) return 0;
-        else if (n % 2 == 0) return n;
-        else
-        {
-            for (int i = 2; i * i < n; i++)
-            {
-                if (n % i == 0) return 0;
-            }
-            return n;
-        }
-
+        
     }
 }
