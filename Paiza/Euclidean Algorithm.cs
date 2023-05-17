dc00b523850336dc5d04605920368cdecd18d89b
MatubaraTooru <kurakura.ingen@gmail.com>
2023-05-17 17:51:38 +0900

    ユークリッドの互除法

---
+++ Paiza/Euclidean Algorithm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class EuclideanAlgorithm
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int f = int.Parse(Console.ReadLine());
+        for (int i = 0; i < n - 1; i++)
+        {
+            f = GCD(f, int.Parse(Console.ReadLine()));
+        }
+
+        Console.WriteLine(f);
+    }
+
+    static int GCD(int a, int b)
+    {
+        if (b == 0)
+        {
+            return a;
+        }
+
+        return GCD(b, a % b);
+    }
+}
--- Paiza/any 2.cs
+++ Paiza/any 2.cs
@@ -5,28 +5,88 @@ class Any2
 {
     static void Main()
     {
-        long n = int.Parse(Console.ReadLine());
-        long maxnum = 0;
-        long num1 = 0;
-        long num2 = 0;
+        var data = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+        int n = data[0];
+        int m = data[1];
+        string[] strArray = new string[data[0]];
 
-        for (long i = 2; i * i < n; i++)
+        for (int i = 0; i < data[0]; i++)
         {
-            
+            strArray[i] = Console.ReadLine();
         }
-    }
-    static long IsPrime(long n)
-    {
-        if (n < 2) return 0;
-        else if (n % 2 == 0) return n;
+
+        bool isLineSymmetry = LineSymmetry1(n, m, strArray) || LineSymmetry2(n, m, strArray);
+
+        bool isPointSymmetry = PointSymmetry(n, m, strArray);
+
+        if (isLineSymmetry && isPointSymmetry)
+        {
+            Console.WriteLine("line point symmetry");
+        }
+        else if (isLineSymmetry)
+        {
+            Console.WriteLine("line symmetry");
+        }
+        else if (isPointSymmetry)
+        {
+            Console.WriteLine("point symmetry");
+        }
         else
         {
-            for (int i = 2; i * i < n; i++)
+            Console.WriteLine("none");
+        }
+    }
+
+    private static bool PointSymmetry(int n, int m, string[] strArray)
+    {
+        bool isPointSymmetry = true;
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
             {
-                if (n % i == 0) return 0;
+                if (strArray[i][j] != strArray[n - i - 1][m - j - 1])
+                {
+                    isPointSymmetry = false;
+                }
             }
-            return n;
         }
 
+        return isPointSymmetry;
+    }
+
+    private static bool LineSymmetry1(int n, int m, string[] strArray)
+    {
+        bool isLineSymmetry = true;
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (strArray[i][j] != strArray[i][m - j - 1])
+                {
+                    isLineSymmetry = false;
+                }
+            }
+        }
+
+        return isLineSymmetry;
+    }
+
+    private static bool LineSymmetry2(int n, int m, string[] strArray)
+    {
+        bool isLineSymmetry = true;
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (strArray[j][i] != strArray[n - j - 1][i])
+                {
+                    isLineSymmetry = false;
+                }
+            }
+        }
+
+        return isLineSymmetry;
     }
 }
