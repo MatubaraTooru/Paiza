fa8e7be05b8991cf8f97b79910b66936a8a10996
MatubaraTooru <kurakura.ingen@gmail.com>
2023-06-25 22:58:06 +0900

    マージソート　シェルソート

---
+++ Paiza/Merge Sort.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+
+class MergeSort
+{
+    private static int count = 0;
+    static void Main()
+    {
+        int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        MergeSortAlgorithm(array);
+        Console.WriteLine(string.Join(" ", array));
+    }
+
+    static void MergeSortAlgorithm(int[] array)
+    {
+        MergeSortHelper(array, 0, array.Length);
+    }
+
+    static void MergeSortHelper(int[] array, int start, int end)
+    {
+        if (end - start > 1)
+        {
+            int middle = (start + end) / 2;
+
+            MergeSortHelper(array, start, middle);
+            MergeSortHelper(array, middle, end);
+
+            Merge(array, start, middle, end);
+        }
+    }
+
+    static void Merge(int[] array, int start, int middle, int end)
+    {
+        int[] temp = new int[end - start];
+        int i = start;
+        int j = middle;
+        int k = 0;
+
+        while (i < middle && j < end)
+        {
+            if (array[i] <= array[j])
+            {
+                temp[k] = array[i];
+                i++;
+            }
+            else
+            {
+                temp[k] = array[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < middle)
+        {
+            temp[k] = array[i];
+            i++;
+            k++;
+        }
+
+        while (j < end)
+        {
+            temp[k] = array[j];
+            j++;
+            k++;
+        }
+
+        for (int m = 0; m < temp.Length; m++)
+        {
+            array[start + m] = temp[m];
+        }
+    }
+}
---
+++ Paiza/Shell Sort.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+class ShellSort
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        int k = int.Parse(Console.ReadLine());
+        int[] h = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        ShellSortAlgorithm(a, h, k);
+    }
+
+    static void ShellSortAlgorithm(int[] a, int[] h, int k)
+    {
+        for (int i = 0; i < k; i++)
+        {
+            InsertionSort(a, h[i]);
+        }
+    }
+
+    static void InsertionSort(int[] a, int h)
+    {
+        int num_of_move = 0;
+
+        for (int i = h; i < a.Length; i++)
+        {
+            int x = a[i];
+            int j = i - h;
+
+            while (j >= 0 && a[j] > x)
+            {
+                a[j + h] = a[j];
+                j -= h;
+                num_of_move++;
+            }
+
+            a[j + h] = x;
+        }
+
+        Console.WriteLine(num_of_move);
+    }
+}
