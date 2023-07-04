04d37a7db0a629a89ca0487250f4c3cbeb635696
MatubaraTooru <kurakura.ingen@gmail.com>
2023-07-04 13:38:09 +0900

    DFS

--- Paiza/Test.cs
+++ Paiza/Test.cs
@@ -1,46 +1,68 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 
 class Program
 {
+    static List<int>[] adList;
+    static List<int> maxTrail = new List<int>();
+    static int maxPasscount = 0;
+
     static void Main()
     {
-        int n = int.Parse(Console.ReadLine());
-        int[] a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        string[] input = Console.ReadLine().Split();
+        int n = int.Parse(input[0]);
+        int s = int.Parse(input[1]);
+        int t = int.Parse(input[2]);
+        int p = int.Parse(input[3]);
 
-        int k = int.Parse(Console.ReadLine());
-        int[] h = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        adList = new List<int>[n + 1];
 
-        ShellSort(a, h, k);
-    }
-
-    static void ShellSort(int[] a, int[] h, int k)
-    {
-        for (int i = 0; i < k; i++)
+        for (int i = 1; i <= n; i++)
         {
-            InsertionSort(a, h[i]);
+            int v = int.Parse(Console.ReadLine());
+            adList[i] = Console.ReadLine().Split().Select(int.Parse).ToList();
         }
+
+        DFS(s, p, t, new List<int> { s }, new List<List<int>>());
+        Console.WriteLine(string.Join(" ", maxTrail));
     }
 
-    static void InsertionSort(int[] a, int h)
+    static void DFS(int v, int point, int target, List<int> trail, List<List<int>> edges)
     {
-        int num_of_move = 0;
-
-        for (int i = h; i < a.Length; i++)
+        foreach (int i in adList[v])
         {
-            int x = a[i];
-            int j = i - h;
+            List<int> e = new List<int> { i, v };
+            e.Sort();
+            int pcount = 0;
 
-            while (j >= 0 && a[j] > x)
+            if (!edges.Any(x => x.SequenceEqual(e)))
             {
-                a[j + h] = a[j];
-                j -= h;
-                num_of_move++;
+                trail.Add(i);
+                edges.Add(e);
+
+                if (i == target)
+                {
+                    foreach (int x in trail)
+                    {
+                        if (x == point)
+                        {
+                            pcount++;
+                        }
+                    }
+                    if (maxTrail.Count < trail.Count)
+                    {
+                        maxTrail = new List<int>(trail);
+                    }
+                }
+                
+                DFS(i, point, target, trail, edges);
+
+                edges.Remove(e);
+
+                trail.RemoveAt(trail.Count - 1);
             }
-
-            a[j + h] = x;
         }
-
-        Console.WriteLine(num_of_move);
     }
 }
